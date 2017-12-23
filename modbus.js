310c551487c6b212d5e444e33964300be61b790f
MarcinH <horas@o2.pl>
2017-12-23 22:54:56 +0100

    make aplikation more stabile (aded conectionn close function)

--- modbus.js
+++ modbus.js
@@ -3,25 +3,25 @@ var dzien;
 var miesiac;
 var rok;
 var godzina;
-var minuta ;
+var minuta;
 var sekunda;
 var dzisiaj;
 
 
 
-function time(){
-dzisiaj = new Date();
-           dzien = dzisiaj.getDate();
-           miesiac = dzisiaj.getMonth()+1;
-          rok = dzisiaj.getFullYear();
-           godzina = dzisiaj.getHours();
-          if (godzina<10) godzina ="0"+godzina;
-         minuta = dzisiaj.getMinutes();
-          if (minuta<10) minuta  = "0"+minuta;
-           sekunda = dzisiaj.getSeconds();
-          if (sekunda<10) sekunda  = "0"+sekunda;
-        //  document.getElementById("zegar").innerHTML = dzien+"/"+miesiac+"/"+rok+" | "+godzina+":"+minuta+":"+sekunda;
-        //  setTimeout("odliczanie()",1000);
+function time() {
+  dzisiaj = new Date();
+  dzien = dzisiaj.getDate();
+  miesiac = dzisiaj.getMonth() + 1;
+  rok = dzisiaj.getFullYear();
+  godzina = dzisiaj.getHours();
+  if (godzina < 10) godzina = "0" + godzina;
+  minuta = dzisiaj.getMinutes();
+  if (minuta < 10) minuta = "0" + minuta;
+  sekunda = dzisiaj.getSeconds();
+  if (sekunda < 10) sekunda = "0" + sekunda;
+  //  document.getElementById("zegar").innerHTML = dzien+"/"+miesiac+"/"+rok+" | "+godzina+":"+minuta+":"+sekunda;
+  //  setTimeout("odliczanie()",1000);
 }
 
 
@@ -35,106 +35,117 @@ let lastreg;
 let regNumber;
 let buttonClicked;
 
-function regTempFuncB4(){
+function regTempFuncB4() {
   buttonClicked = "b4";
-// console.log(buttonClicked);
+  // console.log(buttonClicked);
 
 }
-function regTempFuncB5(){
+
+function regTempFuncB5() {
   buttonClicked = "b5";
 
-// console.log(buttonClicked);
+  // console.log(buttonClicked);
 }
-function regTempFuncB6(){
+
+function regTempFuncB6() {
   buttonClicked = "b6";
 
-// console.log(buttonClicked);
+  // console.log(buttonClicked);
 }
-function regTempFuncB7(){
+
+function regTempFuncB7() {
   buttonClicked = "b7";
 
-// console.log(buttonClicked);
+  // console.log(buttonClicked);
 }
 
 
 
 
 
-function modbus(){
-time()
-ipNumber = document.getElementById("ipAddress").value;
+function modbusWrite() {
+  time()
+  ipNumber = document.getElementById("ipAddress").value;
+
+
+  //////////warunki przepisujace odpowiednia warsoc do zmiennej reg i val
+  if (buttonClicked == "b4") {
+    reg = document.getElementById("regNumberButton4").value;
+    val = document.getElementById("valueButton4").value;
+  }
+  if (buttonClicked == "b5") {
+    reg = document.getElementById("regNumberButton5").value;
+    val = document.getElementById("valueButton5").value;
+  }
+
+  if (buttonClicked == "b6") {
+    reg = document.getElementById("regNumberButton6").value;
+    val = document.getElementById("valueButton6").value;
+  }
+
+  if (buttonClicked == "b7") {
+    reg = document.getElementById("regNumberButton7").value;
+    val = document.getElementById("valueButton7").value;
+  }
+
+
+
 
   let alertMesage = 'ModBus module is not connected\n Please connect the cable, set propper IP and try again';
 
   if (msg === "host 192.168.0.3 is dead") alert(alertMesage, "MH Modbus TCP tester"); //warunek sprawdza czy połacznei jest nieaktywne. Jesli tak to wyśiwtla komunikat, leśłi jest ok to jedzie dalej
   else {
+
     var net = require('net');
 
 
-var modbus = require('jsmodbus');
+    var modbus = require('jsmodbus');
 
-// create a modbus client
-var client = modbus.client.tcp.complete({
-        'host'              : ipNumber,
-        'port'              : 502,
-        'autoReconnect'     : false,
-        'reconnectTimeout'  : 1000,
-        'timeout'           : 5000,
-        'unitId'            : 0
+    // create a modbus client
+    var client = modbus.client.tcp.complete({
+      'host': ipNumber,
+      'port': 502,
+      'autoReconnect': false,
+      'reconnectTimeout': 1000, //1000 domyslene
+      'timeout': 5000,  //5000 domyślne
+      'unitId': 0
     });
 
-client.connect();
+    client.connect();
 
-// reconnect with client.reconnect()
+    // reconnect with client.reconnect()
 
-client.on('connect', function () {
+    client.on('connect', function() {
 
-    // make some calls
+      // make some calls
 
-//////////warunki przepisujace odpowiednia warsoc do zmiennej reg i val
+      client.writeSingleRegister(reg, val).then(function(resp) {
 
-if (buttonClicked == "b4") {
-  reg =document.getElementById("regNumberButton4").value;
-  val =document.getElementById("valueButton4").value;
+        // resp will look like { fc: 6, byteCount: 4, registerAddress: 13, registerValue: 42 }
+        console.log(resp);
+
+      }, console.error).finally(function () {
+        client.close()
+      });
+
+
+
+    });
+  }
 }
-if (buttonClicked == "b5") {
-  reg =document.getElementById("regNumberButton5").value;
-  val =document.getElementById("valueButton5").value; }
-
-if (buttonClicked == "b6") {
-  reg =document.getElementById("regNumberButton6").value;
-  val =document.getElementById("valueButton6").value;
-}
-
-if (buttonClicked == "b7") {
-  reg =document.getElementById("regNumberButton7").value;
-val =document.getElementById("valueButton7").value;
-}
-
-
-    client.writeSingleRegister(reg, val).then(function (resp) {
-
-	    // resp will look like { fc: 6, byteCount: 4, registerAddress: 13, registerValue: 42 }
-	    console.log(resp);
-
-    }, console.error);
-
-
-});
-}}
 
 
 //////////////////////Read Holding register function
-function modbusRead(){
-time()
-firstReg = document.getElementById("firstRegNumber").value;
-lastreg = document.getElementById("lastRegNumber").value;
-let firstRegNumber = Number(firstReg);
-let lastregNumber = Number(lastreg);
+function modbusRead() {
+  time()
+  firstReg = document.getElementById("firstRegNumber").value;
+  lastreg = document.getElementById("lastRegNumber").value;
+  let firstRegNumber = Number(firstReg);
+  let lastregNumber = Number(lastreg);
 
-regNumber = lastreg - firstReg + 1; // liczba rejestrów do odczytu wg wpisanych od XX do XX
+  regNumber = lastreg - firstReg + 1; // liczba rejestrów do odczytu wg wpisanych od XX do XX
 
-ipNumber = document.getElementById("ipAddress").value;
+  ipNumber = document.getElementById("ipAddress").value;
 
   let alertMesage = 'ModBus module is not connected\n Please connect the cable, set propper IP and try again';
 
@@ -145,49 +156,53 @@ ipNumber = document.getElementById("ipAddress").value;
 
     // create a modbus client
     var client = modbus.client.tcp.complete({
-            'host'              : ipNumber,
-            'port'              : 502,
-            'autoReconnect'     : false,
-            'reconnectTimeout'  : 1000,
-            'timeout'           : 5000,
-            'unitId'            : 0
-        });
+      'host': ipNumber,
+      'port': 502,
+      'autoReconnect': false,
+      'reconnectTimeout': 1000,
+      'timeout': 5000,
+      'unitId': 0
+    });
 
     client.connect();
 
     // reconnect with client.reconnect()
 
-    client.on('connect', function () {
+    client.on('connect', function() {
 
-        // make some calls
-
-
-    client.readHoldingRegisters(firstReg, regNumber).then(function (resp) {
-
-           // resp will look like { fc: 3, byteCount: 20, register: [ values 0 - 10 ], payload: <Buffer> }
-           console.log(resp);
-let regNumberView = [];
- regNumberView = resp.register;  //przepisanie wartości tablicy resp do zmiennej roboczej regNumberView
-
- ////////////////pętla dodajaca przedrostek Rx= gdzie x to nr rejestru
-           for (let x=0; x<resp.register.length; x++) {
-
-               regNumberView[x] = ["R"+(firstRegNumber+ x)+"="+resp.register[x]];
-           }
+      // make some calls
 
 
-let separator = ";";
-           document.getElementById("viewResult").innerHTML += godzina +":"+ minuta+":" + sekunda +": "+ "Receive:" + regNumberView.join(separator) +"\n"; //resp.register powoduje wyświetlenie danych z tablicy
+      client.readHoldingRegisters(firstReg, regNumber).then(function(resp) {
 
-       }, console.error);
-     })}}
+        // resp will look like { fc: 3, byteCount: 20, register: [ values 0 - 10 ], payload: <Buffer> }
+        console.log(resp);
+        let regNumberView = [];
+        regNumberView = resp.register; //przepisanie wartości tablicy resp do zmiennej roboczej regNumberView
 
-     function clearTextaArea(){
+        ////////////////pętla dodajaca przedrostek Rx= gdzie x to nr rejestru
+        for (let x = 0; x < resp.register.length; x++) {
 
-     //document.getElementById("viewResult").innerHTML += godzina +":"+ minuta+":" + sekunda +": "+"Receive:" +"&nbsp" + data3 +"\n\n";
-     document.getElementById("viewResult").innerHTML = "";
+          regNumberView[x] = ["R" + (firstRegNumber + x) + "=" + resp.register[x]];
+        }
 
-     }
+
+        let separator = ";";
+        document.getElementById("viewResult").innerHTML += godzina + ":" + minuta + ":" + sekunda + ": " + "Received:" + regNumberView.join(separator) + "\n"; //resp.register powoduje wyświetlenie danych z tablicy
+
+      }, console.error).finally(function () {
+        client.close()
+      });
+    })
+  }
+}
+
+function clearTextaArea() {
+
+  //document.getElementById("viewResult").innerHTML += godzina +":"+ minuta+":" + sekunda +": "+"Receive:" +"&nbsp" + data3 +"\n\n";
+  document.getElementById("viewResult").innerHTML = "";
+
+}
 
 // client.on('error', function (err) {
 //
